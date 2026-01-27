af2ed14db2006ac82c2b5cc0dcb6083f02c6df09
Harshwardhan Manjare <harshwardhanmanjarem5@gmail.com>
2026-01-27 20:44:06 +0530

    Initialised

---
+++ SanguinSync/nearbyHosp/nearbyhosp.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "This is nearbyHosp" << std::endl;
+    return 0;
+}
---
+++ SanguinSync/rewards/rewards.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "This is rewards" << std::endl;
+    return 0;
+}
