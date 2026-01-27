90acb88be2f46d0c9fa54b6f12a74e0690fcc83f
Harshwardhan Manjare <harshwardhanmanjarem5@gmail.com>
2026-01-27 21:12:49 +0530

    first login page added

--- SanguinSync/loginPage/loginPage.cpp
+++ SanguinSync/loginPage/loginPage.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+using namespace std;
+
 int main() {
-    std::cout << "This is loginPage" << std::endl;
+    char choice;
+
+    cout << "================================" << endl;
+    cout << "   Welcome to Sanguine Sync!!   " << endl;
+    cout << "================================" << endl;
+    cout << endl;
+    cout << "       Who is signing in?       " << endl;
+    cout << "1. User" << endl;
+    cout << "2. Hospital" << endl;
+    cout << endl;
+    cin >> choice;
+
+    switch (choice) {
+        case '1':
+            cout << "User login selected." << endl;
+            // userLogin();
+            break;
+
+        case '2':
+            cout << "Hospital login selected." << endl;
+            // hospitalLogin();
+            break;
+
+        default:
+            cout << "Invalid choice. Please select 1 or 2." << endl;
+            break;
+    }
+
     return 0;
 }
