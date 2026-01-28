620d6f39a781111cf7e20c65d98ccc70c794a3f5
Harshwardhan Manjare <harshwardhanmanjarem5@gmail.com>
2026-01-28 20:51:52 +0530

    User Sign In Added

--- SanguinSync/loginPage/loginPage.cpp
+++ SanguinSync/loginPage/loginPage.cpp
@@ -1,26 +1,99 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    char choice;
+// User
+class User {
+public:
+    string username;
+    string password;
 
-    cout << "================================" << endl;
-    cout << "   Welcome to Sanguine Sync!!   " << endl;
-    cout << "================================" << endl;
+    User(string u, string p) {
+        username = u;
+        password = p;
+    }
+};
+
+// UserSignIn
+class UserSignIn {
+public:
+    static void userLogin(User users[], int size) {
+        cout << "=======================================" << endl;
+        cout << "     WELCOME TO USER SIGN-IN SYSTEM    " << endl;
+        cout << "=======================================" << endl;
+        cout << endl;
+
+        string inputUsername, inputPassword;
+
+        cout << "Enter Username: ";
+        getline(cin, inputUsername);
+
+        cout << "Enter Password: ";
+        getline(cin, inputPassword);
+
+        bool usernameFound = false;
+        bool passwordCorrect = false;
+
+        for (int i = 0; i < size; i++) {
+            if (users[i].username == inputUsername) {
+                usernameFound = true;
+                if (users[i].password == inputPassword) {
+                    passwordCorrect = true;
+                }
+                break;
+            }
+        }
+
+        if (!usernameFound) {
+            cout << endl;
+            cout << "[ERROR] Username not found!" << endl;
+            cout << "Please check your username and try again." << endl;
+        }
+        else if (!passwordCorrect) {
+            cout << endl;
+            cout << "[ERROR] Incorrect password!" << endl;
+            cout << "Please try again." << endl;
+        }
+        else {
+            cout << endl;
+            cout << "=======================================" << endl;
+            cout << "           LOGIN SUCCESSFUL!           " << endl;
+            cout << "=======================================" << endl;
+            cout << "Welcome, " << inputUsername << "!" << endl;
+            cout << "You have successfully signed in." << endl;
+            cout << "=======================================" << endl;
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    int choice;
+
+    User users[4] = {
+        User("User1", "User1@123"),
+        User("User2", "User2@123"),
+        User("User3", "User3@123"),
+        User("User4", "User4@123")
+    };
+
+    cout << "=======================================" << endl;
+    cout << "             SANGUINE SYNC             " << endl;
+    cout << "=======================================" << endl;
     cout << endl;
     cout << "       Who is signing in?       " << endl;
     cout << "1. User" << endl;
     cout << "2. Hospital" << endl;
     cout << endl;
     cin >> choice;
-
+    cin.ignore(); // Buffer
+    
     switch (choice) {
-        case '1':
-            cout << "User login selected." << endl;
-            // userLogin();
+        case 1:
+            UserSignIn::userLogin(users, 4);
             break;
 
-        case '2':
+        case 2:
             cout << "Hospital login selected." << endl;
             // hospitalLogin();
             break;
